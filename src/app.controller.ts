f7a6f5e3c282038ddf726d4d2d0e46695cd66c95
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-11-24 08:49:33 -0300

    adiciona suporte para streaming de vídeo com verificação de existência e testes e2e

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,18 +3,26 @@ import {
   Body,
   Controller,
   Get,
+  Header,
   HttpCode,
   HttpStatus,
+  NotFoundException,
+  Param,
   Post,
   Req,
+  Res,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
+
+import type { Request, Response } from 'express';
+
 import { AppService } from './app.service';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { randomUUID } from 'node:crypto';
-import { extname } from 'node:path';
+import path, { extname } from 'node:path';
+import fs from 'node:fs';
 import { PrismaService } from '@src/prisma.service';
 
 @Controller()
@@ -93,6 +101,51 @@ export class AppController {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+    });
+  }
+
+  @Get('stream/:videoId')
+  @Header('Content-Type', 'video/mp4')
+  async streamVideo(
+    @Param('videoId') videoId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<any> {
+    const video = await this.prismaService.video.findUnique({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video) {
+      throw new NotFoundException('Video not found');
+    }
+
+    const filePath = path.join('.', video.url);
+    const fileSize = fs.statSync(filePath).size;
+
+    const range = req.headers.range;
+    if (range) {
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunksize = end - start + 1;
+      const file = fs.createReadStream(filePath, { start, end });
+
+      res.writeHead(HttpStatus.PARTIAL_CONTENT, {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      });
+
+      return file.pipe(res);
+    }
+
+    res.writeHead(HttpStatus.OK, {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
     });
   }
 }
